da33ce23f678d648c3f1356b82a85d4236a35a18
Javier Arroliga <jarrolig@charlotte.edu>
2025-09-27 15:18:07 -0400

    Event cards on event page expand when clicked on

--- frontend/scripts/events.js
+++ frontend/scripts/events.js
@@ -2,16 +2,16 @@ const eventDropdown = document.getElementById("eventDropdown");
 const cardsDiv = document.getElementById("cardsDiv");
 
 const events = [
-    { name: "Event 1", sport: "soccer", date: "2023-10-01", time: "10:00 AM" , location: "Stadium A" },
-    { name: "Event 2", sport: "basketball", date: "2023-10-05", time: "2:00 PM" , location: "Arena B" },
-    { name: "Event 3", sport: "tennis", date: "2023-10-10", time: "11:00 AM" , location: "Court C" },
-    { name: "Event 4", sport: "baseball", date: "2023-10-15", time: "4:00 PM" , location: "Stadium D" },
-    { name: "Event 5", sport: "football", date: "2023-10-20", time: "1:00 PM" , location: "Arena E" }
+    { name: "Event 1", sport: "soccer", date: "2023-10-01", time: "10:00 AM" , location: "Stadium A", description: "lorem ipsum" },
+    { name: "Event 2", sport: "basketball", date: "2023-10-05", time: "2:00 PM" , location: "Arena B", description: "lorem ipsum" },
+    { name: "Event 3", sport: "tennis", date: "2023-10-10", time: "11:00 AM" , location: "Court C", description: "lorem ipsum" },
+    { name: "Event 4", sport: "baseball", date: "2023-10-15", time: "4:00 PM" , location: "Stadium D", description: "lorem ipsum" },
+    { name: "Event 5", sport: "football", date: "2023-10-20", time: "1:00 PM" , location: "Arena E", description: "lorem ipsum" },
 ];
 
 let filteredEvents;
 
-function defaultView() {
+function renderCards(events) {
     let content = "";
     for (let i = 0; i < events.length; i++) {
         content += `
@@ -21,36 +21,35 @@ function defaultView() {
                 <p>Date: ${events[i].date}</p>
                 <p>Time: ${events[i].time}</p>
                 <p>Location: ${events[i].location}</p>
+                <div class="details"> ${events[i].description}</div>
             </div>
         `;
     }
     cardsDiv.innerHTML = content;
+
+    const cards = document.querySelectorAll('.game-card');
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('expanded');
+        });
+    });
+}
+
+function defaultView() {
+    renderCards(events);
 }
 
 defaultView();
 
 function filter(string) {
-    filteredEvents = events.filter(event => event.sport === string);
+    if (string === "all") {
+        filteredEvents = events;
+    } else {
+        filteredEvents = events.filter(event => event.sport === string);
+        renderCards(filteredEvents);
+    }
 }
 
 eventDropdown.addEventListener("change", function() {
-    let selectedSport = eventDropdown.value;
-    if (selectedSport === "all") {
-        defaultView();
-    } else {
-        filter(selectedSport);
-        let content = "";
-        for (let i = 0; i < filteredEvents.length; i++) {
-            content += `
-            <div class="game-card">
-                <h3>${filteredEvents[i].name}</h3>
-                <p>Sport: ${filteredEvents[i].sport}</p>
-                <p>Date: ${filteredEvents[i].date}</p>
-                <p>Time: ${filteredEvents[i].time}</p>
-                <p>Location: ${filteredEvents[i].location}</p>
-            </div>
-        `;
-        }
-        cardsDiv.innerHTML = content;
-    }
+    filter(eventDropdown.value);
 });
