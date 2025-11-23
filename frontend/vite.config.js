392184833d6fc93ae73e80c5d4ef80c99d21b8f8
LandonRipper <landonripper@hotmail.com>
2025-11-23 16:08:54 -0500

    changed some things to allow for deployment of website to gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  
+  base: command === "build" ? "/4155_Project/" : "/",
+
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
   },
-});
+}));
